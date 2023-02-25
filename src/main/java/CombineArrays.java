60f08d47cc6ecd57f8f978fd8a641bdc6c19e203
kevin kennedy <kjkennedy96@gmail.com>
2023-02-25 04:53:38 +0000

    Completed Challenge Combine Arrays

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -9,6 +9,22 @@ public class CombineArrays {
      */
     public int[] combine(int[] arr1, int[] arr2){
 
-        return null;
+        int l1 = arr1.length;
+        int l2 = arr2.length;
+        int l3 = l1 + l2;
+
+        int [] arr3 = new int [l3];
+        for(int x = 0; x < l1; x++){
+            arr3[x] = arr1[x];
+        }
+        for(int x = 0; x < l2; x++){
+            arr3[x+l1] = arr2[x];
+        }
+        for (int i = 0; i < l3; i = i + 1) {
+              
+            
+            System.out.println(arr3[i]);
+        }
+        return arr3;
     }
 }
