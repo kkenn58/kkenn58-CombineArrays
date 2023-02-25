235ea2f3404de1af202429bc830c02f13c2a10e4
kevin kennedy <kjkennedy96@gmail.com>
2023-02-25 04:54:18 +0000

    Completed Challenge Combine Arrays

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -20,11 +20,7 @@ public class CombineArrays {
         for(int x = 0; x < l2; x++){
             arr3[x+l1] = arr2[x];
         }
-        for (int i = 0; i < l3; i = i + 1) {
-              
-            
-            System.out.println(arr3[i]);
-        }
+    
         return arr3;
     }
 }
